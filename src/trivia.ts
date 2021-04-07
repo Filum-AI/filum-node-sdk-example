481ed4a23091f4287e529c6a49bd6b4d82b1843e
hiepnp-filum <hiep@filum.ai>
2021-04-07 15:23:00 +0700

    feat: Add example code for Filum NodeJS client

---
+++ src/trivia.ts
@@ -0,0 +1,33 @@
+var Analytics = require('filum-node-sdk');
+
+const client = new Analytics('<YOUR WRITE KEY PASTE HERE>')
+
+for (let i = 0; i < 10; i++) {
+    const your_user_id = String('Testing User ' + i)
+    client.identify({
+        user_id: your_user_id,
+        event_name: 'Identify',
+        event_params: {
+            'name': String("Mock User " + i),
+            'phone': '0987654321',
+            'address': 'Saigon, Vietnam'
+        }
+
+    })
+    for (let i = 0; i < 10; i++) {
+        client.track({
+            user_id: your_user_id,
+            event_name: 'Item Purchased',
+            event_params: {
+                name: "Testing item",
+                stock: i,
+                price: i + 1.5
+            }
+        });
+    }
+}
+    
+
+client.flush()
+
+console.log ("Done sending events to Event API!");
